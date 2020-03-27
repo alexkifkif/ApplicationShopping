036974f5c1de4f7962b34e93eec2d20f50426cd9
alexkifkif <59054724+alexkifkif@users.noreply.github.com>
2020-03-27 14:03:31 +0100

    Add files via upload

---
+++ ShoppingTest/obj/Debug/netstandard2.0/Pages/Caracteristics.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShoppingTest;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ShoppingTest.Services;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Microsoft.CSharp.RuntimeBinder;
+
+namespace ShoppingTest.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Caracteristics : ContentPage
+    {
+        public Caracteristics()
+        {
+            InitializeComponent();
+
+            ServiceStorage.Products = new ObservableCollection<Product>();
+        }
+
+        //Liste chaque caractéristique du produit
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            LabelName.Text = ServiceStorage.SelectedProduct.Name;
+            LabelPrice.Text = ServiceStorage.SelectedProduct.Price.ToString();
+            LabelQuantity.Text = ServiceStorage.SelectedProduct.Quantity.ToString();
+            LabelBrand.Text = ServiceStorage.SelectedProduct.Brand;
+            LabelPlace.Text = ServiceStorage.SelectedProduct.Place;
+        }
+
+        private void ListofDetails_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+
+        }
+
+        private void ListofDetails_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ ShoppingTest/obj/Debug/netstandard2.0/Pages/Listing.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShoppingTest.Services;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShoppingTest.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Listing : ContentPage
+    {        
+        public Listing()
+        {
+            InitializeComponent();
+
+            ServiceStorage.Products = new ObservableCollection<Product>();
+        }
+
+        //Bouton qui retourne vers la page où on enregistre les produits
+        public async void OnToolbarClicked(object sender, EventArgs e)
+        {   
+            await Navigation.PushAsync(new MainPage());
+        }
+
+        //Liste les produits enregistrés
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var productList = await App.Database.GetAllProductAsync();
+
+            if (productList != null)
+            {
+                ListofItems.ItemsSource = productList;
+            }
+        }
+
+        //Lorsqu'on clique sur l'item on navigue vers une page avec ses caractéristiques
+        private void ListofItems_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            ServiceStorage.SelectedProduct =  (Product)e.SelectedItem;
+
+            Navigation.PushAsync(new Caracteristics());
+        }
+        
+        //Methode de suppression dans le menu du swipe
+        private async void MenuItem_Clicked(object sender, EventArgs e)
+        {
+            var menuItem = (MenuItem)sender;
+
+            Product product = (Product)menuItem.CommandParameter;
+
+            await App.Database.DeleteProductAsync(product);
+            
+            await DisplayAlert("Success", "Produit supprimé", "OK");
+
+            var productList = await App.Database.GetAllProductAsync();
+
+            if (productList != null)
+            {
+                ListofItems.ItemsSource = productList;
+            }
+        }
+    }
+}
