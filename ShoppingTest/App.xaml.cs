f1c66f332ea1a97b1666397c62a8823422443227
alexkifkif <59054724+alexkifkif@users.noreply.github.com>
2020-03-27 13:57:49 +0100

    Add files via upload

---
+++ ShoppingTest/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ShoppingTest.Services;
+using ShoppingTest.Pages;
+
+namespace ShoppingTest
+{
+    public partial class App : Application
+    {
+
+        static ProductDatabase database;
+
+        public static ProductDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new ProductDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ProductDatabase.db3"));
+                }
+                return database;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Listing());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
