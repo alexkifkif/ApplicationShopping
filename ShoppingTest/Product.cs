b02dce114019ba0184c6fe74f454ca8d8f8d0560
alexkifkif <59054724+alexkifkif@users.noreply.github.com>
2020-03-27 14:00:27 +0100

    Add files via upload

---
+++ ShoppingTest/Product.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+
+namespace ShoppingTest
+{
+    public class Product
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public string Place { get; set; }
+        public string Brand { get; set; }
+        public double TotalPrice
+        {
+            get { return Quantity * Price; }
+        }
+        public string Temperature { get; set; }
+    }
+}
